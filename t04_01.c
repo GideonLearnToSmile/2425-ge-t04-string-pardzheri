4f3c38913844d0423e3836a9dbb6046ff1abce12
GideonLearnToSmile <deonpanjaitan010@gmail.com>
2025-03-05 11:13:10 +0700

    Sudah

--- t04_01.c
+++ t04_01.c
@@ -1,10 +1,26 @@
-// NIM - Name
-// NIM - Name
-
 #include <stdio.h>
+#include <string.h>
 
-int main(int _argv, char **_argc) {
-  // codes
+int main(void) {
+    char kata[60];
+    scanf("%s", kata);
 
-  return 0;
+    int panjang = strlen(kata) / 3;
+    int daftar[panjang];
+
+    for (int i = 0; i < panjang; i++) {
+        // Konversi manual dari string ke integer
+        int angka = (kata[i * 3] - '0') * 100 + 
+                    (kata[i * 3 + 1] - '0') * 10 + 
+                    (kata[i * 3 + 2] - '0');
+
+        daftar[i] = angka;  // Simpan hasil konversi
+    }
+
+    // Cetak hasil sebagai karakter ASCII
+    for (int i = 0; i < panjang; i++) {
+        printf("%c", daftar[i]);
+    }
+
+    return 0;
 }
--- t04_02.c
+++ t04_02.c
@@ -2,9 +2,17 @@
 // NIM - Name
 
 #include <stdio.h>
+#include <string.h>
 
 int main(int _argv, char **_argc) {
-  // codes
+
+char kata[20];
+fgets(kata, 20, stdin);
+kata[strlen(kata) - 1] = '\0';
+int panjang = strlen(kata);
+
+for (int i = 0; i < panjang ; i++)
+{printf("%03d",kata[i]);}
 
   return 0;
 }
