7666ce4e3812541fcd2eca2b2eaf79aca5c70684
GideonLearnToSmile <deonpanjaitan010@gmail.com>
2025-03-05 11:29:21 +0700

    commit

--- t04_02.c
+++ t04_02.c
@@ -8,10 +8,15 @@ int main(int _argv, char **_argc) {
 
 char kata[20];
 fgets(kata, 20, stdin);
-kata[strlen(kata) - 1] = '\0';
 int panjang = strlen(kata);
+if (kata[panjang - 1] == '\n')
+{
+  kata[panjang - 1] = '\0';
+  panjang--
+}
 
-for (int i = 0; i <= panjang ; i++)
+
+for (int i = 0; i < panjang ; i++)
 {printf("%03d",kata[i]);}
 
   return 0;
