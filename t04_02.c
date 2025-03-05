0a6aefa4583cfd1d0faa286e2e93e74afcaa45f1
GideonLearnToSmile <deonpanjaitan010@gmail.com>
2025-03-05 11:44:00 +0700

    sd

--- t04_02.c
+++ t04_02.c
@@ -1,27 +1,22 @@
+// Gideon Panjaitan
+// 11S24036
+
 #include <stdio.h>
 #include <string.h>
 
 int main(int _argv, char **_argc) {
-    char kata[20];
-    fgets(kata, 20, stdin);
 
-    // Hapus newline (`\n`) jika ada
-    size_t panjang = strlen(kata);
-    if (panjang > 0 && kata[panjang - 1] == '\n') {
-        kata[panjang - 1] = '\0';
-        panjang--; // Update panjang string
-    }
+char kata[20];
+fgets(kata, 20, stdin);
+int panjang = strlen(kata);
+if (kata[panjang - 1] == '\r')
+{
+  kata[panjang - 1] = '\0';
+}
 
-    // Hapus carriage return (`\r`) jika ada (untuk Windows)
-    if (panjang > 0 && kata[panjang - 1] == '\r') {
-        kata[panjang - 1] = '\0';
-        panjang--;
-    }
 
-    // Cetak ASCII dengan format 3 digit
-    for (size_t i = 0; i < panjang; i++) {
-        printf("%03d", kata[i]);
-    }
+for (int i = 0; i <= panjang ; i++)
+{printf("%03d",kata[i]);}
 
-    return 0;
+  return 0;
 }
